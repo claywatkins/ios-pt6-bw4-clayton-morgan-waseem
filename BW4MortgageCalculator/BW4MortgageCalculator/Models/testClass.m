7d8742aa02a6a6d04ba46500168acca19f4078d0
Clayton Watkins <watkinclayton17@yahoo.com>
2020-10-13 18:50:34 -0700

    Inital Commit : Project Created and scaffolded. Bridging header created. Target updated to reflect moving of info.plist and Bridging header

---
+++ BW4MortgageCalculator/BW4MortgageCalculator/Models/testClass.m
@@ -0,0 +1,14 @@
+//
+//  testClass.m
+//  BW4MortgageCalculator
+//
+//  Created by Clayton Watkins on 10/13/20.
+//
+
+#import "testClass.h"
+
+@implementation testClass
+
+// For information, see .h file
+
+@end
