fde068e2a5656b34f558c7f44458b12cd9aecb3b
Waseem Idelbi <idelbiw@gmail.com>
2020-10-19 23:22:36 -0400

    added the detail view logic in the detail view controller file and added all the UI elements in storyboard

--- BW4MortgageCalculator/BW4MortgageCalculator/ViewControllers/MortgageDetailViewController.swift
+++ BW4MortgageCalculator/BW4MortgageCalculator/ViewControllers/MortgageDetailViewController.swift
@@ -8,9 +8,54 @@
 import UIKit
 
 class MortgageDetailViewController: UIViewController {
-
+    
+    //MARK: - Properties and IBOutlets -
+    
+    // Properties
+    var mortgage: Mortgage? {
+        didSet {
+            updateViews()
+        }
+    }
+    let initialNickname = ""
+    
+    // IBOutlets
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var totalCostLabel: UILabel!
+    @IBOutlet weak var monthlyPaymentLabel: UILabel!
+    @IBOutlet weak var principalLabel: UILabel!
+    @IBOutlet weak var downPaymentLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var termLabel: UILabel!
+    
+    //MARK: - Methods and IBActions -
+    
+    // Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+//        initialNickname = mortgage.nickname
     }
     
-}
+    func updateViews() {
+        guard let mortgage = mortgage else { return }
+//        title = mortgage.nickname
+//        nicknameTextField.text = mortgage.nickname
+        totalCostLabel.text = "\(mortgage.totalCost)"
+        monthlyPaymentLabel.text = "\(mortgage.monthlyPayment)"
+        principalLabel.text = "\(mortgage.principal)"
+        downPaymentLabel.text = "\(mortgage.downPayment)"
+        rateLabel.text = "\(mortgage.interestRate)"
+        termLabel.text = "\(mortgage.term)"
+    }
+    
+    // IBActions
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func nicknameTextFieldValueChanged(_ sender: UITextField) {
+        saveButton.isEnabled = true
+    }
+    
+} //End of class
