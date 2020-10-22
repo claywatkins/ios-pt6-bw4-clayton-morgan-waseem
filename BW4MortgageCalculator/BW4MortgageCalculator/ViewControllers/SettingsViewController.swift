6c50085688522ffc8215df6af510b4d04ad4c3e2
Clayton Watkins <watkinclayton17@yahoo.com>
2020-10-22 14:57:36 -0700

    Created SettingsViewController on storyboard and respective view controller

---
+++ BW4MortgageCalculator/BW4MortgageCalculator/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  BW4MortgageCalculator
+//
+//  Created by Clayton Watkins on 10/22/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    // MARK: - Properties
+    
+    // MARK: - IBActions
+
+}
