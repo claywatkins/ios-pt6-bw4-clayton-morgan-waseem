e5cf7cad122c76b7f4a5b4d56591404b072185f9
Waseem Idelbi <idelbiw@gmail.com>
2020-10-23 00:22:20 -0400

    made it more efficient to show user's location, if it has already been fetched, the app will just show the location that was fetched rather than fetching it again

--- BW4MortgageCalculator/BW4MortgageCalculator/ViewControllers/MapViewController.swift
+++ BW4MortgageCalculator/BW4MortgageCalculator/ViewControllers/MapViewController.swift
@@ -52,6 +52,12 @@ class MapViewController: UIViewController {
     }
     
     func fetchUserLocation() {
+        
+        if userAnnotation != nil {
+            showUserLocation()
+            return
+        }
+        
         present(fetchingAlert, animated: true)
         fetchingAlertIsPresent = true
         let locationManager = CLLocationManager()
